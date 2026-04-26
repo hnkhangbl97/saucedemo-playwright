6d27fbc46bf2ddd08d48ea04d4f15344924fc8f5
khangnguyen <hnkhangbl97@gmail.com>
2026-04-26 19:04:13 +0700

    Init project

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import { BASE_URL } from './configs/sauce-demo-config';
 
 /**
  * Read environment variables from file.
@@ -26,8 +27,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
+    baseURL: BASE_URL,
+    testIdAttribute: 'data-test',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
