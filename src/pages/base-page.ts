9f20a416f19cb50a88a0bc21f8e5229041cd8444
khangnguyen <hnkhangbl97@gmail.com>
2026-05-13 20:06:18 +0700

    feat: create pom for footer test cases

--- src/pages/base-page.ts
+++ src/pages/base-page.ts
@@ -2,11 +2,30 @@ import { Page, Locator, expect } from '@playwright/test';
 
 export class BasePage {
     protected page: Page;
-
+    readonly socialXIcon: Locator;
+    readonly socialFacebookIcon: Locator;
+    readonly socialLinkedinIcon: Locator;
+    readonly footerCopyText: Locator;
     
     constructor(page: Page) {
         this.page = page;
+        this.socialXIcon = page.getByTestId('social-twitter');
+        this.socialFacebookIcon = page.getByTestId('social-facebook');
+        this.socialLinkedinIcon = page.getByTestId('social-linkedin');
+        this.footerCopyText = page.getByTestId('footer-copy');
     }
 
+    async clickToSocialXIcon(){
+        await this.socialXIcon.click();
+    }
 
+    async clickToSocialFacebookICon(){
+        await this.socialFacebookIcon.click();
+    }
+
+    async clickToSocialLinkedinIcon(){
+        await this.socialLinkedinIcon.click();
+    }
+
+    
 }
